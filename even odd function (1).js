63f646ec05fe42316aedb151f26004b9cc19a23f
Rup saha <rahulsaha01903853283@gmail.com>
2024-10-18 22:59:59 +0600

    Add files via upload

---
+++ even odd function (1).js
@@ -0,0 +1,11 @@
+function checkEvenOrOdd(number) {
+  if (number % 2 === 0) {
+    console.log(number + " is even.");
+  } else {
+    console.log(number + " is odd.");
+  }
+}
+
+// Example usage:
+checkEvenOrOdd(7);  // Output: "7 is odd."
+checkEvenOrOdd(10); // Output: "10 is even."
